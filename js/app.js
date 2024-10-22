7a4fa7c34ea1f05ec462b3a579bea39a5117ddf3
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-22 16:25:41 +0200

    ADD init in js and ternary operator

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 const myButton = document.getElementById("switchLamp");
 const myDiv = document.getElementById("image-container");
 
+// Lo stato della mia lampadina
+let lampStatus = false;
+
+// Inizializzare la mia applicazione
+// Metto l'immagine della lampadina
+// Metto il testo del bottone
+
+myDiv.style.backgroundImage = lampStatus === false ? "url('../img/white_lamp.png')" : "url('../img/yellow_lamp.png')";
+myButton.innerText = lampStatus === false ? "Accendi" : "Spegni";
+
+
 myButton.addEventListener('click', function () {
-    if(myDiv.classList.contains("off")){
-        myDiv.classList.add('on');
-        myDiv.classList.remove('off');
-        myDiv.style.backgroundImage = "url('../img/yellow_lamp.png')"
-        myButton.innerText="Spegni";
-    }
-    else {
-        myDiv.classList.add('off');
-        myDiv.classList.remove('on');
-        myDiv.style.backgroundImage = "url('../img/white_lamp.png')"
-        myButton.innerText="Accendi";
-    }
+    myDiv.style.backgroundImage = lampStatus === false ? "url('../img/white_lamp.png')" : "url('../img/yellow_lamp.png')";
+    myButton.innerText = lampStatus === false ? "Accendi" : "Spegni";
+    lampStatus = lampStatus === false ? true : false;
 });
