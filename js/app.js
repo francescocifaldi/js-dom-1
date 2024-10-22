ce8bbfca9fa4f3a9d284c86fe7cb18f9f1f63c61
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-22 17:29:09 +0200

    ADD optimization with variables

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 const myButton = document.getElementById("switchLamp");
 const myDiv = document.getElementById("image-container");
 
+const IMG_LAMP_ON = '../img/yellow_lamp.png';
+const IMG_LAMP_OFF = '../img/white_lamp.png';
+
+const MSG_LAMP_ON = 'ON';
+const MSG_LAMP_OFF = 'OFF';
+
+
 // Lo stato della mia lampadina
-let lampStatus = false;
+let lampStatus = true;
 
 // Inizializzare la mia applicazione
 // Metto l'immagine della lampadina
 // Metto il testo del bottone
 
-myDiv.style.backgroundImage = lampStatus === false ? "url('../img/white_lamp.png')" : "url('../img/yellow_lamp.png')";
-myButton.innerText = lampStatus === false ? "Accendi" : "Spegni";
-
+myDiv.style.backgroundImage = "url('" + (!lampStatus ? IMG_LAMP_OFF : IMG_LAMP_ON) + "')";
+myButton.innerText = !lampStatus ? MSG_LAMP_ON  : MSG_LAMP_OFF;
 
 myButton.addEventListener('click', function () {
-    myDiv.style.backgroundImage = lampStatus === false ? "url('../img/white_lamp.png')" : "url('../img/yellow_lamp.png')";
-    myButton.innerText = lampStatus === false ? "Accendi" : "Spegni";
-    lampStatus = lampStatus === false ? true : false;
+    lampStatus = !lampStatus;
+    myDiv.style.backgroundImage = "url('" + (!lampStatus ? IMG_LAMP_OFF : IMG_LAMP_ON) + "')";
+    myButton.innerText = !lampStatus ? MSG_LAMP_ON  : MSG_LAMP_OFF;
 });
